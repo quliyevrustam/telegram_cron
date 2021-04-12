04684ba7041739ed66a41afc556e30b20e1ba24b
Rustam <quliyev.rustam@gmail.com>
2021-04-12 12:14:01 +0000

    RSS Part II

--- cron/rss.php
+++ cron/rss.php
@@ -1,30 +1,55 @@
 <?php
 
-if (!file_exists('madeline.php')) {
-    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+require_once dirname(__DIR__ ). '/vendor/autoload.php';
+require_once (dirname(__DIR__ ).'/config/di.config.php');
+
+if (!file_exists(dirname(__DIR__) . '/cron/madeline/session/madeline.php')) {
+    copy('https://phar.madelineproto.xyz/madeline.php', dirname(__DIR__) . '/cron/madeline/session/madeline.php');
 }
-include 'madeline.php';
+include dirname(__DIR__) . '/cron/madeline/session/madeline.php';
 
-$MadelineProto = new \danog\MadelineProto\API('session.madeline');
-$MadelineProto->async(true);
-$MadelineProto->loop(function () use ($MadelineProto) {
-    yield $MadelineProto->start();
+try {
+    $settings['app_info']['api_id'] = APP_API_ID;
+    $settings['app_info']['api_hash'] = APP_API_HASH;
+    $madelineProto = new \danog\MadelineProto\API('madeline/session/telegram.session.madeline',$settings);
+    $madelineProto->start();
 
-    $me = yield $MadelineProto->getSelf();
+    $channels = [
+        'azeri_word_by_day',
+        'jurefuck',
+        'zangezuravtonomia',
+        'voice_of_turkey',
+        'nataosmanli',
+        'dayaz',
+        'operativmm',
+    ];
+    $offset_id = 0;
+    $limit = 1;
 
-    $MadelineProto->logger($me);
+    foreach ($channels as $channel)
+    {
+        $request = $madelineProto->messages->getHistory(['peer' => $channel, 'offset_id' => $offset_id, 'offset_date' => 0, 'add_offset' => 0, 'limit' => $limit, 'max_id' => 0, 'min_id' => 0, 'hash' => 0 ]);
+        if(count($request['messages']) > 0)
+        {
+            foreach ($request['messages'] as $message)
+            {
+                $replyCount = (isset($message['replies']['replies'])) ? $message['replies']['replies'] : 0;
 
-    if (!$me['bot']) {
-        yield $MadelineProto->messages->sendMessage(['peer' => '@danogentili', 'message' => "Hi!\nThanks for creating MadelineProto! <3"]);
-        yield $MadelineProto->channels->joinChannel(['channel' => '@MadelineProto']);
+                echo 'channel '.$channel."\n";
+                echo 'id '.$message['id']."\n";
+                echo 'date '.$message['date']."\n";
+                echo 'views '.$message['views']."\n";
+                echo 'forwards '.$message['forwards']."\n";
+                echo 'replies '.$replyCount."\n";
+                echo 'message '.mb_substr($message['message'], 0, 100)."..."."\n";
 
-        try {
-            yield $MadelineProto->messages->importChatInvite(['hash' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg']);
-        } catch (\danog\MadelineProto\RPCErrorException $e) {
-            $MadelineProto->logger($e);
+                echo "\n"."\n";
+
+                //print_r($message);
+            }
         }
-
-        yield $MadelineProto->messages->sendMessage(['peer' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg', 'message' => 'Testing MadelineProto!']);
     }
-    yield $MadelineProto->echo('OK, done!');
-});
+}
+catch (\danog\MadelineProto\Exception $e) {
+    echo $e->getMessage();
+}
